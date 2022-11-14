6b01b3c9697b9ea2f0142d05ebf833a35bb36afb
Aymen EL OUAGOUTI <aymen.elouagouti@gmail.com>
2022-11-14 19:11:38 +0100

    Add basics and uml things to Personne

--- src/Personne.java
+++ src/Personne.java
@@ -1,3 +1,26 @@
 //Une personne peut être un élève(utilisateur) ou un professeur
 public abstract class Personne {
+    private String nom;
+    private String prenom;
+
+    Personne(String nom, String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 }
