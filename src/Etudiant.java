d53220b90b1ec2f296ae147eb6f39365bd56a178
aymen.elouagouti <aymen.el-ouagouti@etu.univ-nantes.fr>
2022-11-15 14:31:24 +0100

    Removed listEvenements

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -4,14 +4,13 @@ public class Etudiant extends Personne {
     private ArrayList<Stats> statsPerso;
     private ArrayList<Stats> statsProfs;
     private ArrayList<Stats> statsMatiere;
-    private ArrayList<Stats> listEvenement;
+
 
     public Etudiant(String nom, String prenom) {
         super(nom, prenom);
         this.statsPerso = new ArrayList<Stats>();
         this.statsProfs = new ArrayList<Stats>();
         this.statsMatiere = new ArrayList<Stats>();
-        this.listEvenement = new ArrayList<Stats>();
     }
 
     public ArrayList<Stats> getStatsPerso() {
@@ -36,13 +35,5 @@ public class Etudiant extends Personne {
 
     public void setStatsMatiere(float prctg, int i) {
         this.statsMatiere.get(i).valeur = this.statsMatiere.get(i).valeur + (this.statsMatiere.get(i).valeur * prctg);
-    }
-
-    public ArrayList<Stats> getListEvenement() {
-        return listEvenement;
-    }
-
-    public void setListEvenement(ArrayList<Stats> listEvenement) {
-        this.listEvenement = listEvenement;
     }
 }
