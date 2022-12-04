5dbf62ce023ed9f01d10b80f528ec123bc469f75
Maty DIOP <maty.diop@etu.univ-nantes.fr>
2022-12-04 15:43:34 +0100

    add comments in Pause

--- src/main/java/studentlife/core/events/Pause.java
+++ src/main/java/studentlife/core/events/Pause.java
@@ -4,6 +4,8 @@ import studentlife.core.Matiere;
 import studentlife.core.characters.Etudiant;
 import static studentlife.Config.*;
 
+/**
+ * {@literal la classe Pause implemente l'interface Evenement et modifie les stats des objets impliqués}*/
 public class Pause implements Evenement {
 
     private PauseType pauseType;
@@ -24,8 +26,13 @@ public class Pause implements Evenement {
        this.pauseType = pauseType;
     }
 
-
-
+/**
+ * @param utilisateur les stats de user seront modifiés en fonction de la pause qu'il a pris ainsi:
+ * _ lorsque c'est une pause repas, la faim diminue, la fatigue aussi et ainsi l'attention augmente.
+ * _ lorsque c'est une pause repos, la faim reste la meme, la fatigue diminue et l'attention augmente.
+ * _ lorsque c'est une pause revision, la fatigue augmente, l'attention diminue mais la moyenne de la matière revisé augmente de 10.
+ * @param valid booleen qui verifie si l'evenement choisi est bien un Cours, ainsi la procedure pourra modifier les stats.
+ * */
     @Override
     public void finaliserEvenement(Etudiant utilisateur, boolean valid) {
         switch (pauseType) {
@@ -50,7 +57,12 @@ public class Pause implements Evenement {
         }
     }
 
-
+/**
+ * @param utilisateur la moyenne de l'utilisateur dans la matiere sera modifié
+ * si la matière est mentionné la moyenne se verra augmenté de 10.
+ * @throws RuntimeException si la matière n'existe pas ou n'est pas mentionée,
+ * l'exception fais propagé un message d'erreur.
+ * */
     private void faireRevision(Etudiant utilisateur) throws RuntimeException{
             if (subject == null){
                 throw new RuntimeException("Il faut une matière à réviser");
