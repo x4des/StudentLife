7c9f5c84e773ed53032973f8a0b73a1f030e8538
Maty DIOP <maty.diop@etu.univ-nantes.fr>
2022-12-04 14:59:23 +0100

    add comments

--- src/main/java/studentlife/core/events/Evenement.java
+++ src/main/java/studentlife/core/events/Evenement.java
@@ -1,10 +1,14 @@
 package studentlife.core.events;
 
 import studentlife.core.characters.Etudiant;
-
-//Un Evenement correspond au differentes "taches" de la journée de l'étudiant
+/**
+ * {@literal Un evenement correspond au différentes "taches" fera ou pas dans la journée
+ * cette interface sera donc implémenter par les classes Cours et Pause qui eux, modifieront les stats
+ * de l'utilsateur selon le choix de l'evenement (aller en cours ou faire une pause)}*/
 public interface Evenement {
-
-    //method "finaliser" param: etudiant, choix pour changer ses stats
+    /**
+     * @param utilisateur les stats de l'etudiant seront changées selon le choix de l'evenemnt
+     * @param valid  un boolern qui verifie que le choix de l'utlisateur est bien celui du meme evenement
+     * */
     public void finaliserEvenement(Etudiant utilisateur, boolean valid);
 }
