9bb8e793c7a4aa2dadad1e8aaae2c488edf1d0e5
Maty DIOP <maty.diop@etu.univ-nantes.fr>
2022-12-04 21:48:39 +0100

    comment added

--- src/main/java/studentlife/core/Day.java
+++ src/main/java/studentlife/core/Day.java
@@ -4,27 +4,46 @@ import studentlife.core.events.Evenement;
 
 import java.util.ArrayList;
 /**
- * {@literal La classe jour represente un jour dans la semaine composé d'une liste d'evenements}
+ *La classe jour represente un jour dans la semaine composé d'une liste d'evenements
  * */
 
 public class Day {
     private ArrayList<Evenement> evenements;
 
+    /**
+     * constructeur de la classe Day, jour sans evenements
+     * */
     public Day() {
         this.evenements = new ArrayList<>();
     }
 
+    /**
+     * @param evenements
+     * constructeur d'un jour avec plusieurs evenements
+     * */
     public Day(ArrayList<Evenement> evenements) {
         this.evenements = evenements;
     }
 
+    /**
+     * @return retourne un evenement
+     * getter qui permet d'acceder aux evenements
+     * */
     public ArrayList<Evenement> getEvenements() {
         return evenements;
     }
 
+    /**
+     * @param evenements
+     * setter modifie la valeur de la liste d'evenements
+     * */
     public void setEvenements(ArrayList<Evenement> evenements) {
         this.evenements = evenements;
     }
 
+    /**
+     * @param evenement
+     * ajoute un evenement a la liste d'evenement
+     * */
     public void addEvenement(Evenement evenement) { evenements.add(evenement); }
 }
--- src/main/java/studentlife/core/Schedule.java
+++ src/main/java/studentlife/core/Schedule.java
@@ -3,7 +3,7 @@ package studentlife.core;
 import java.util.ArrayList;
 
 /**
- * {@literal La classe Schedule represente l'emploi du temps de la semaine.
+ *La classe Schedule represente l'emploi du temps de la semaine.
  Elle est composé d'une liste de jour
  * @see Day.java
  * }
@@ -16,17 +16,27 @@ public class Schedule {
     private ArrayList<Day> week;
 
 
-
+    /**
+     * Premier constructeur de Schedule
+     * */
     public Schedule() {
         this.week = new ArrayList<>();
         this.weekDays = new String[]{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"};
     }
 
+    /**
+     * Deuxieme constructeur
+     * @param csvPath le nom d'un fichier csv, charge/rempli le EDT a partir du fichier csv.
+     * */
     public Schedule(String csvPath) {
         this.weekDays =new String[]{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"};
         this.loadEDT(csvPath);
     }
 
+    /**
+     * @param day un jour dans la semaine
+     * ajoute un jour dans la semaine lorsque l'EDT n'est pas complet (<5 jours)
+     * */
     public void addDay(Day day) {
 
         if(week.size() >= MAX_DAYS)
@@ -35,10 +45,17 @@ public class Schedule {
         week.add(day);
     }
 
+    /**
+     * @param i  indice du jour souhaité dans le weekDays
+     * @return retrourne le jour dont l'indice est passé en parametre
+     * */
     public String getWeekday(int i){
         return weekDays[i];
     }
 
+    /**
+     * @return retourne la liste de jour de la semaine
+     * */
     public ArrayList<Day> getWeek() {
         return week;
     }
