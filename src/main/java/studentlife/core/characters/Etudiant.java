46c23ad9601f5f45266fe402ac6f90932a7949e9
Maty DIOP <maty.diop@etu.univ-nantes.fr>
2022-12-04 21:01:15 +0100

    add comments

--- src/main/java/studentlife/core/characters/Etudiant.java
+++ src/main/java/studentlife/core/characters/Etudiant.java
@@ -1,18 +1,34 @@
 package studentlife.core.characters;
 
 import static studentlife.Config.*;
-
+/**
+ *  la classe Etudiant represente un etudiant/utilisateur qui est une personne.
+ Cette classe descend de la classe Personne
+ qui possède des stats. Ses stats, seront tout au long de la simulation, modifier
+ selon les choix que l'utilisateur. Ses statistiques personnelles sont composés de
+ la faim, la fatigue, l'attention.
+ * @see Personne.java
+ * */
 public class Etudiant extends Personne {
 
     private StatsManager stats;
 
+    /**
+     * @param nom
+     * @param prenom
+     * constructeur d'un Etudiant
+     * */
     public Etudiant(String nom, String prenom) {
         super(nom, prenom);
         initStats();
     }
 
+    /**
+     * @return ce getter permet d'acceder à l'attribut stat de la classe
+     * */
     public StatsManager getStats() { return stats; }
 
+    // permet l'affichage d'un etudiant
     @Override
     public String toString() {
 
@@ -20,7 +36,7 @@ public class Etudiant extends Personne {
     }
 
 
-    //initialise le StatsManager avec les 3 stats perso
+    /**initialise le StatsManager avec les 3 stats perso*/
     private void initStats() {
         stats = new StatsManager();
         stats.setStat(STAT_ATTENTION, 100);
--- src/main/java/studentlife/core/characters/Personne.java
+++ src/main/java/studentlife/core/characters/Personne.java
@@ -1,23 +1,39 @@
 package studentlife.core.characters;
 
+/**
+ * la classe represente une personne
+ qui posséde un nom et un prenom
+ * */
 //Une personne peut être un élève(utilisateur) ou un professeur
 public class Personne {
     private final String nom;
     private final String prenom;
 
+    /**
+     * @param nom
+     * @param prenom
+     * le constructeur de la classe Personne
+     * */
     public Personne(String nom, String prenom){
         this.nom = nom;
         this.prenom = prenom;
     }
-
+    /**
+     * @return le getter retourne l'attribut nom d'un objet Personne
+     * */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * @return le getter retourne l'attribut prenom d'un objet Personne
+     * */
     public String getPrenom() {
         return prenom;
     }
-
+    /**
+     * @return retourn un sting permettant d'afficher une personne ( son nom et prenom)
+     * */
     @Override
     public String toString(){
         return (this.nom)+" "+this.prenom;
